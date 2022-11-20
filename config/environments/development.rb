0e2ed82a559bc11feda4243b8ca6494f2569bf51
tetsu <forlearninghsk@gmail.com>
2022-11-20 16:11:40 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'a572788aad33408d8345858abba18883.vfs.cloud9.us-east-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
